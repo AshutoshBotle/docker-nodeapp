380c88348f3588fab69a983c0b5dad04be14f720
AshutoshBotle <Ashutosh@123>
2025-01-21 10:50:55 +0000

    updateddd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Welcome to new endddddd');
+ res.send('Welcome to Ashutosh World');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
